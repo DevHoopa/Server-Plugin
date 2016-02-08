33be8a44cdb1edebf5b3a13313d32d3fd992b328
Flaflo <TheFlaflo@yahoo.de>
2016-02-08 14:32:18 +0100

    Supertool: "Super Schaufel" hinzugefügt

--- TestNCPServer/src/de/flaflo/command/CommandItem.java
+++ TestNCPServer/src/de/flaflo/command/CommandItem.java
@@ -20,17 +20,22 @@ import org.bukkit.inventory.ItemStack;
 public class CommandItem implements CommandExecutor {
 
 	public static final Map<Enchantment, Integer> SUPER_AXE_ECHANTMENTS;
-	
+	public static final Map<Enchantment, Integer> SUPER_SHOVEL_ECHANTMENTS;
+
 	static {
 		SUPER_AXE_ECHANTMENTS = new HashMap<Enchantment, Integer>();
-		
-		final ItemStack dummyStack = new ItemStack(Material.DIAMOND_PICKAXE, 1);
-		
+		SUPER_SHOVEL_ECHANTMENTS = new HashMap<Enchantment, Integer>();
+
+		final ItemStack axeDummy = new ItemStack(Material.DIAMOND_PICKAXE);
+		final ItemStack shovelDummy = new ItemStack(Material.DIAMOND_SPADE);
+
 		for (Enchantment ench : Enchantment.values())
-			if (ench.canEnchantItem(dummyStack))
+			if (ench.canEnchantItem(axeDummy))
 				SUPER_AXE_ECHANTMENTS.put(ench, ench.getMaxLevel());
+			else if (ench.canEnchantItem(shovelDummy))
+				SUPER_SHOVEL_ECHANTMENTS.put(ench, ench.getMaxLevel());
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] args) {
 		Player p = (Player) arg0;
@@ -61,14 +66,18 @@ public class CommandItem implements CommandExecutor {
 			}
 		} else if (args.length == 1) {
 			if (args[0].equalsIgnoreCase("superaxe")) {
-				ItemStack superAxe = new ItemStack(Material.DIAMOND_PICKAXE, amount);
-				try {
-					superAxe.addEnchantments(SUPER_AXE_ECHANTMENTS);
-					p.getInventory().addItem(superAxe);
-					p.sendMessage("§7[§aItem§7]§r §aDiese Spitzhacke bewirkt wunder!");
-				} catch (Exception ex) {
-					p.sendMessage(ex.getMessage());
-				}
+				ItemStack superAxe = new ItemStack(Material.DIAMOND_PICKAXE);
+				superAxe.addEnchantments(SUPER_AXE_ECHANTMENTS);
+				
+				p.getInventory().addItem(superAxe);
+				p.sendMessage("§7[§aItem§7]§r §aDiese Spitzhacke bewirkt wunder!");
+			}
+			if (args[0].equalsIgnoreCase("superaxe")) {
+				ItemStack superShovel = new ItemStack(Material.DIAMOND_SPADE);
+				superShovel.addEnchantments(SUPER_SHOVEL_ECHANTMENTS);
+				
+				p.getInventory().addItem(superShovel);
+				p.sendMessage("§7[§aItem§7]§r §aDiese Schaufel bewirkt wunder!");
 			} else {
 				Material material = null;
 
